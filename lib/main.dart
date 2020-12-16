6e373b0d53a103e40abd0d1c2fb68d9dc729ea0d
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-12-15 21:16:45 -0300

    inicialização do app shop

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './views/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Minha Loja',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
---
+++ lib/views/products_overview_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
+import '../models/product.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Minha Loja'),
+        ),
+        body: GridView.builder(
+            itemCount: loadedProducts.length,
+            padding: const EdgeInsets.all(10),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemBuilder: (ctx, i) => Text(loadedProducts[i].title)));
+  }
+}
+
+//SliverGridDelegateWithFixedCrossAxisCount significa que vai ter uma quantidade fixas de elementos no eixo CrossAxis(Linha)
