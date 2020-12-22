efdfa3555c5ec5d2408aff755de0f708d0e60aff
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-12-21 21:45:37 -0300

    criaçao do multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,22 @@ import './views/products_overview_screen.dart';
 import './utils/app_routes.dart';
 import './views/product_detail.screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => new Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => new Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => new Cart(),
+        ),
+      ],
       child: MaterialApp(
           title: 'Minha Loja',
           theme: ThemeData(
