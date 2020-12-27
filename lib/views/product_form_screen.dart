b9fa4cfb5cbd7e76408a63d290570bb2092b685b
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-12-27 20:49:50 -0300

    criando a tela de gerenciamento de produtos

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -6,11 +6,36 @@ class ProductFormScreen extends StatefulWidget {
 }
 
 class _ProductFormScreenState extends State<ProductFormScreen> {
+  final _priceFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Product form'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocusNode,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
