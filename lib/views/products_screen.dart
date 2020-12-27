a6bd746418340c063db4c90de441bf96065d9052
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-12-27 20:34:07 -0300

    criando a tela de gerenciamento de produtos

--- lib/views/products_screen.dart
+++ lib/views/products_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/products.dart';
+import 'package:shop/widgets/product_item.dart';
 
 class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final products = Provider.of<Products>(context);
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Manage Products'),
@@ -22,8 +24,13 @@ class ProductsScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-          itemCount: products.itemsCount,
-          itemBuilder: (ctx, i) => Text('teste'),
+          itemCount: productsData.itemsCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              ProductItem(products[i]),
+              Divider(),
+            ],
+          ),
         ),
       ),
     );
