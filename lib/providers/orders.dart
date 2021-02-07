fce966ede261151bc95eac60a83e7e472ce2fde4
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-02-06 21:16:26 -0300

    loading orders via the FutureBuilder widget

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
-
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -3,37 +3,44 @@ import 'package:provider/provider.dart';
 import '../providers/orders.dart';
 import '../widgets/order_widget.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    Provider.of<Orders>(context, listen: false).loadOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
   Widget build(BuildContext context) {
-    final Orders orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('My orders'),
       ),
-      body: _isLoading
-          ? Center(
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, i) => OrderWidget(orders.items[i]),
-              itemCount: orders.itemsCount,
-            ),
+            );
+          } else if (snapshot.error != null) {
+            return Center(
+              child: Text('An error has occurred!'),
+            );
+          } else {
+            return Consumer<Orders>(
+              builder: (ctx, orders, child) {
+                return ListView.builder(
+                  itemBuilder: (ctx, i) => OrderWidget(orders.items[i]),
+                  itemCount: orders.itemsCount,
+                );
+              },
+            );
+          }
+        },
+      ),
+      // _isLoading
+      // ? Center(
+      //     child: CircularProgressIndicator(),
+      //   )
+      //     : ListView.builder(
+      //         itemBuilder: (ctx, i) => OrderWidget(orders.items[i]),
+      //         itemCount: orders.itemsCount,
+      //       ),
     );
   }
 }
