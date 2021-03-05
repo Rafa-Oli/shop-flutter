5208319a8cad39685177a344522db90fc56ae747
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-03-05 19:56:18 -0300

    correcting responsiveness

--- lib/views/auth_screen.dart
+++ lib/views/auth_screen.dart
@@ -21,39 +21,48 @@ class AuthScreen extends StatelessWidget {
             ),
           ),
         ),
-        Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 20.0),
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 70),
-                transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                // '..' cascade operator
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blue,
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 8,
-                      color: Colors.black26,
-                      offset: Offset(0, 2),
-                    ),
-                  ],
-                ),
-                child: Text(
-                  'My Store',
-                  style: TextStyle(
-                    color: Theme.of(context).accentTextTheme.headline6.color,
-                    fontSize: 45,
-                    fontFamily: "Anton",
+        Center(
+          child: SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 50,
                   ),
-                ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20.0),
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 70),
+                    transform: Matrix4.rotationZ(-8 * pi / 180)
+                      ..translate(-10.0),
+                    // '..' cascade operator
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 8,
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Text(
+                      'My Store',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).accentTextTheme.headline6.color,
+                        fontSize: 45,
+                        fontFamily: "Anton",
+                      ),
+                    ),
+                  ),
+                  AuthCard(),
+                ],
               ),
-              AuthCard(),
-            ],
+            ),
           ),
         ),
       ],
