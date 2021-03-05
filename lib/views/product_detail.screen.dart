66ae8ba6c03bf144ba5a6e10fdbf96d86f4e6f4e
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-03-05 20:42:47 -0300

    working with slivers and improving layout

--- lib/views/product_detail.screen.dart
+++ lib/views/product_detail.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../providers/product.dart';
 
 class ProductDetailScreen extends StatelessWidget {
@@ -7,43 +8,72 @@ class ProductDetailScreen extends StatelessWidget {
     final Product product =
         ModalRoute.of(context).settings.arguments as Product;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                product.imageUrl,
-                fit: BoxFit.cover,
+      // appBar: AppBar(
+      //   title: Text(product.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          // area que vc pode ter scroll
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.title),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Hero(
+                    tag: product.id,
+                    child: Image.network(
+                      product.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment(0, 0.8),
+                            end: Alignment(0, 0),
+                            colors: [
+                          Color.fromRGBO(0, 0, 0, 0.6),
+                          Color.fromRGBO(0, 0, 0, 0),
+                        ])),
+                  )
+                ],
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'R\$ ${product.price}',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate(
+            [
+              SizedBox(
+                height: 10,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              width: double.infinity,
-              child: Text(
-                product.description,
+              Text(
+                'R\$ ${product.price}',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
                 textAlign: TextAlign.center,
               ),
-            ),
-          ],
-        ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                width: double.infinity,
+                child: Text(
+                  product.description,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 1000,
+              )
+            ],
+          ))
+        ],
       ),
     );
   }
